04119c5464f0bba9c685c87fc8e2a2c0023af4de
pradeep597 <pradeep.ml2007@gmail.com>
2024-11-06 13:42:35 +0530

    Delete parvam-cse-pradeep directory

--- parvam-cse-pradeep/nov 5/even.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    for(int i=2;i<=100;++i){
-        printf("%d ",i);
-        i=i+1;
-    } 
-    return 0;
-
-}
--- parvam-cse-pradeep/nov 5/forloop.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    for(int i=2;i<=5;i++){
-        printf("%d ",3);
-    } 
-    return 0;
-
-}
