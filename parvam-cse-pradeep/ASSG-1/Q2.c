245885a72fb4328170fae6e33c433ba657db3085
pradeep597 <pradeep.ml2007@gmail.com>
2024-11-10 14:05:26 +0530

    Delete parvam-cse-pradeep/ASSG-1 directory

--- parvam-cse-pradeep/ASSG-1/Q2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int num;
-    scanf("%x", &num);
-    printf("%5d\n", num);
-    printf("%5o\n", num);
-    printf("%5X\n", num);
-    return 0;
-}
--- parvam-cse-pradeep/ASSG-1/Q5.c
+++
@@ -1,9 +0,0 @@
-
-#include <stdio.h>
-int main() {
-    float num;
-    scanf("%f", &num);
-    printf("%.2f\n", num);  
-    printf("%d\n", (int)num); 
-    return 0;
-}
