7bdb7b2e26b86b09f917183cd41d5c5bbdee482c
pradeep597 <pradeep.ml2007@gmail.com>
2024-11-12 10:44:30 +0530

    Add files via upload

---
+++ parvam-cse-pradeep/nov12/insertion.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        printf("array elements");
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n-1;i++){
+       int j=i-1;
+       int temp=arr[i];
+       while(j>=0 && temp<arr[i]){
+            
+                arr[j+1]=arr[j];
+                j--;
+               
+            }
+            arr[j+1]=temp;
+        }
+    
+    for(int i=0;i<n;i++){
+        printf("%d",arr[i]);
+}
+}
